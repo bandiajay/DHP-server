e23b884c4d4a6806bde1e1078bd39fe2a0c65db4
Sai Deep Konduri <kondurisaideep@gmail.com>
2022-04-24 03:38:07 -0500

    new endpoint

--- src/routes/verifier.js
+++ src/routes/verifier.js
@@ -2,11 +2,12 @@ const express =require("express");
 const { body } = require("express-validator");
 const router = express.Router();
 const { getUserById, isUserSignedIn, isSessionValid, getUserByDHPId, isVerifier } = require("../controllers/auth");
-const { getTransactionById } = require("../controllers/holder");
+const { getTransactionById, getIPFSURL } = require("../controllers/holder");
 
 
 router.param("userId", getUserById);
 router.get('/transaction/:txId/:userId', isUserSignedIn, isSessionValid , getTransactionById);
 
+router.get('/mtransaction/:txId/:userId', isUserSignedIn, isSessionValid, isVerifier , getIPFSURL);
 
 module.exports = router;
